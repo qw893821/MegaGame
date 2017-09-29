0d4b327e62d01b808f142e419630e6bd9a5a3d40
qw893821 <xiuman893821@gmail.com>
2017-09-29 17:11:02 -0400

    add enemy
    add enemy and add enemyhealth script

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum EnemyType{
+    enemy1,
+    enemy2,
+    enemy3,
+    enemy4
+}
+public class EnemyHealth : MonoBehaviour {
+    public float health;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
