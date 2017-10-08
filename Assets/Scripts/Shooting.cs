cd1362dafcf6aa23c74704a7c8e351eebd4b05fb
qw893821 <xiuman893821@gmail.com>
2017-10-08 01:48:26 -0400

    shooting comment add
    add some comment

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -14,14 +14,14 @@ public class Shooting : MonoBehaviour {
     //timer for different stage of charge
     float level1Timer;
     float level2Timer;
-    float normalTimer;
+    //float normalTimer;
 	// Use this for initialization
 	void Start () {
         cannon = GameObject.Find("Cannon");
         time = 0.3f;
         timer = 0;
         chargeTimer = 0f;
-        normalTimer = 0.5f;
+        //normalTimer = 0.5f;
         level1Timer = 1f;
         level2Timer = 2f;
 
@@ -47,16 +47,16 @@ public class Shooting : MonoBehaviour {
             }
                 if (Input.GetButtonUp("Fire1"))
                 {
-                    if (chargeTimer <= level1Timer)
+                    if (chargeTimer <= level1Timer)//time less than level 1 charge time, bullet instantiate normal bullet
                     {
                         Instantiate(bullets[0], bulletPos, Quaternion.identity);
                     }
-                    else if (chargeTimer > level1Timer && chargeTimer <= level2Timer)
+                    else if (chargeTimer > level1Timer && chargeTimer <= level2Timer)//charge time more than level 1 but less than level 2, mid bullet
                     {
                         Instantiate(bullets[1], bulletPos, Quaternion.identity);
                     }
-                    else if (chargeTimer > level2Timer)
-                    {
+                    else if (chargeTimer > level2Timer)//charge time more than level 2, large bullet.
+                {
                         Instantiate(bullets[2], bulletPos, Quaternion.identity);
                     }
                     chargeTimer = 0f;
