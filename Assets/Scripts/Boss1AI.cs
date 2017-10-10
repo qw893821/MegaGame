3c7bd4c1cb6163286a7646883b4b42498225b97a
qw893821 <xiuman893821@gmail.com>
2017-10-10 14:43:19 -0400

    boss shoot
    boss now could shoot normal bullet
    .need add laser

--- Assets/Scripts/Boss1AI.cs
+++ Assets/Scripts/Boss1AI.cs
@@ -8,22 +8,35 @@ public class Boss1AI : MonoBehaviour {
 
     //bullet shoot properity
     float bulletTime;//time between two shot
+    float shootTimer;//record normal shoot time
     float laserChargeTime;//laser charge time
     bool laserMode;
 
+    //shoot bullet &laser
+    public GameObject bullet;
+    public GameObject laser;
+
 	// Use this for initialization
-	void Start () {
-        bossEye = GameObject.Find("Eye");
+	void Awake () {
         laserMode = false;
+        bulletTime = 2f;
+        shootTimer = 0f;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        Shoot();
 	}
 
     void Shoot()
     {
-
+        
+        shootTimer += Time.deltaTime;
+        if (!laserMode&&shootTimer>=bulletTime)
+        {
+            Instantiate(bullet);
+            shootTimer = 0;
+        }
     }
 }
--- Assets/Scripts/EnemyBullet.cs
+++ Assets/Scripts/EnemyBullet.cs
@@ -9,20 +9,33 @@ public class EnemyBullet : MonoBehaviour {
 
     //enemy bullet related properity
     float speed;
+    Vector3 dir;//bullet move direction
 
     //get boss eye posititon
     GameObject eye;
     public Vector3 eyePos;
 
+    //enemy bullet have three directions
+    GameObject bulletM;
+    GameObject bulletL;
+    GameObject bulletR;
+    
+
 	// Use this for initialization
 	void Awake () {
         player = GameObject.FindGameObjectWithTag("Player");
         playerPos = player.transform.position;
-        eye = GameObject.Find("Eye");
+        eye = GameObject.Find("BulletPos");
         eyePos = eye.transform.position;
         speed = 2f;
         transform.position = eyePos - new Vector3(0.5f, 0, 0);
-	}
+        dir = playerPos - transform.position;
+        bulletM = transform.Find("BulletM").gameObject;
+        bulletL = transform.Find("BulletL").gameObject;
+        bulletR = transform.Find("BulletR").gameObject;
+
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -31,7 +44,13 @@ public class EnemyBullet : MonoBehaviour {
 
     void BulletMove()
     {
-        transform.position = Vector3.MoveTowards(transform.position, playerPos, speed * Time.deltaTime);
+        Vector3 dirL;//bullet left direciton
+        Vector3 dirR;//bullet right direction
+        dirL = dir.normalized + new Vector3(-0.6f, 0, 0);
+        dirR = dir.normalized + new Vector3(0.6f, 0, 0);
+        bulletM.transform.position += dir.normalized* speed * Time.deltaTime;
+        bulletL.transform.position += dirL.normalized * speed * Time.deltaTime;
+        bulletR.transform.position += dirR.normalized * speed * Time.deltaTime;
         print(speed * Time.deltaTime);
     }
 }
