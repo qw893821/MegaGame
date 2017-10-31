5322314ebda826ae63458bc11acf0e6ce1341826
qw893821 <xiuman893821@gmail.com>
2017-10-31 13:15:14 -0400

    prefabe change

---
+++ Assets/Scripts/HoriPlate.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoriPlate : MonoBehaviour {
+    //plate move speed
+    float speed;
+    //plate move direction
+    Vector3 dir;
+	// Use this for initialization
+	void Awake () {
+        speed = 4f;
+        dir = new Vector3(0, 1f, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        HoriPlateMovement();
+
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Reverse")
+        {
+            dir =-dir;
+        }
+    }
+
+    void HoriPlateMovement()
+    {
+        transform.position += dir.normalized * Time.deltaTime * speed;
+    }
+}
---
+++ Assets/Scripts/VertPlate.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VertPlate : MonoBehaviour {
+    //plate move speed
+    float speed;
+
+    //plate direction
+    Vector3 dir;
+
+    //get plate
+	// Use this for initialization
+	void Awake () {
+        speed = 4f;
+        dir = new Vector3(1f, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        PlateMovement();
+	}
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Reverse")
+        {
+            dir = -dir;
+            print("enter");
+        }
+    }
+
+    void PlateMovement()
+    {
+        transform.position += dir.normalized * Time.deltaTime * speed;
+    }
+}
